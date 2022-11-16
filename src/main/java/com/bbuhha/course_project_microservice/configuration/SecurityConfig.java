cbadf7dd0278226e4b894d860c3a69992aaa23ef
Buhancev <92110193+Buhancev@users.noreply.github.com>
2022-11-16 21:05:00 +0700

    Added: Swagger, Configurations: SwaggerConfig, SecurityConfig

---
+++ src/main/java/com/bbuhha/course_project_microservice/configuration/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.bbuhha.course_project_microservice.configuration;
+
+import com.bbuhha.course_project_microservice.security.jwt.JwtConfigurer;
+import com.bbuhha.course_project_microservice.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll()
+                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/v1/admin/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+}
--- src/main/java/com/bbuhha/course_project_microservice/service/imlp/PersonServiceImpl.java
+++ src/main/java/com/bbuhha/course_project_microservice/service/imlp/PersonServiceImpl.java
@@ -5,6 +5,7 @@ import com.bbuhha.course_project_microservice.repository.PersonRepository;
 import com.bbuhha.course_project_microservice.repository.RoleRepository;
 import com.bbuhha.course_project_microservice.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class PersonServiceImpl implements PersonService {
     private final RoleRepository repository;
     private final BCryptPasswordEncoder passwordEncoder;
 
+    @Lazy
     @Autowired
     public PersonServiceImpl(PersonRepository personRepository, RoleRepository repository, BCryptPasswordEncoder passwordEncoder) {
 
