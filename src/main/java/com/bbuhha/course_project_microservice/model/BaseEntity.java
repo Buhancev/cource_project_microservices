c8de7b625d4c0cc348db3cbe0b1d8edf281405e9
Buhancev <92110193+Buhancev@users.noreply.github.com>
2022-11-16 19:59:51 +0700

    Added: Repository: Person, Role

--- src/main/java/com/bbuhha/course_project_microservice/model/BaseEntity.java
+++ src/main/java/com/bbuhha/course_project_microservice/model/BaseEntity.java
@@ -8,6 +8,7 @@ import java.util.Date;
 @MappedSuperclass
 @Data
 public class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
---
+++ src/main/java/com/bbuhha/course_project_microservice/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.bbuhha.course_project_microservice.repository;
+
+import com.bbuhha.course_project_microservice.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    public Person findByUsername(String username);
+}
