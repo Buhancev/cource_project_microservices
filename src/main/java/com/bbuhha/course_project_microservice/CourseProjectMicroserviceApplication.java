d3860ec4e3f4efa47f65f83dd430e9364ea2ecb7
Buhancev <92110193+Buhancev@users.noreply.github.com>
2022-11-16 19:34:12 +0700

    Init commit: Empty project

---
+++ src/main/java/com/bbuhha/course_project_microservice/CourseProjectMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bbuhha.course_project_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProjectMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbuhha/course_project_microservice/CourseProjectMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbuhha.course_project_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
